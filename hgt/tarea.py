f3a8f1e869c88d7d02c3eab1c3de74650292c902
fmoorenoo <fermontemore@gmail.com>
2024-05-15 09:41:21 +0100

    Avanzado tarea y evento

---
+++ hgt/tarea.py
@@ -0,0 +1,15 @@
+class Tarea:
+    def __init__(self, id:int, tarea:str, estado:bool) -> None:
+        self.id     = id
+        self.tarea  = tarea
+        self.estado = estado
+
+    # Métodos CRUD
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
