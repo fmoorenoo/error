b914db1008d386cf8497d4e66b83ac9cdff77c32
fmoorenoo <fermontemore@gmail.com>
2024-05-15 09:41:45 +0100

    Avanzado listatareas y listaeventos

---
+++ hgt/listatareas.py
@@ -0,0 +1,13 @@
+class ListaTareas:
+    def __init__(self, lista:list):
+        pass
+
+    # Métodos CRUD
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
